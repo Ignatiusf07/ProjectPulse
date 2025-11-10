03a8eb65e2abddd78181857b991e9a82a6374435
Ignatiusf07 <ignatiuspinto95@gmail.com>
2025-11-10 14:45:02 +0530

    Updated project

--- src/firebase.js
+++ src/firebase.js
@@ -5,17 +5,17 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "#",
-  authDomain: "projectpulse-000702.firebaseapp.com",
-  projectId: "projectpulse-000702",
-  storageBucket: "projectpulse-000702.appspot.com",
-  messagingSenderId: "1038725896874",
-  appId: "1:1038725896874:web:f568a1ba6768a52d5aced3",
-  measurementId: "G-DYD6YY5WJZ"
+  apiKey: "AIzaSyDMruw8ZD9xefSkX_4y1xuv9tdVkF7T-ng",
+  authDomain: "projectpulse-ad152.firebaseapp.com",
+  projectId: "projectpulse-ad152",
+  storageBucket: "projectpulse-ad152.firebasestorage.app",
+  messagingSenderId: "282847671815",
+  appId: "1:282847671815:web:589eb8c4e74d14c797dca2",
+  measurementId: "G-B0Q98B49NG"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
